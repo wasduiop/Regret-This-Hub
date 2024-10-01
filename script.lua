98cb9b021737a1d16f2653fd17682f65daa38019
wasduiop <wasduiop20@gmail.com>
2024-09-30 23:06:50 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 wait(1)
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Regret this Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "RegretThis", IntroEnabled = false, IntroText = "Regret this Hub"})
+local Window = OrionLib:MakeWindow({Name = "Regret this Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "somethingreallyrelevant", IntroEnabled = false, IntroText = "Regret this Hub"})
 
 --[[
 Name = <string> - The name of the UI.
